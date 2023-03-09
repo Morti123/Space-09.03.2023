447673fadec643aab0f32bb66360039684e9070f
Morti123 <114169876+Morti123@users.noreply.github.com>
2023-03-09 18:34:19 +0300

    Add files via upload

---
+++ settings.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+const float WINDOW_WIDTH = 1200;
+const float WINDOW_HEIGHT = 900;
+const std::string WINDOW_TITLE = "SFML Asteroids P53122";
+const float FPS = 60.f;
+
+const std::string Player_File_Name = "images\\spaceShips_008.png";
+const float PI = 3.14159265358;
